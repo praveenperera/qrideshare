64aa1ef0dff6170995c570ee7af5ff34d3abfe3c
Praveen <ppraveen25@gmail.com>
2015-01-28 23:44:38 -0500

    Added validations to Ride#New
    Validations for posting new rides have been added. Refactored adding
    error message to form_without_bootstrap

--- app/helpers/rides_helper.rb
+++ app/helpers/rides_helper.rb
@@ -2,4 +2,24 @@ module RidesHelper
 	def current_user_id
 		current_user.id
 	end
+
+	def errors_for(form,field)
+		content_tag(:span, "#{Ride.human_attribute_name(field)} #{form.object.errors[field].try(:first)}" , class: 'help-block') if form.object.errors[field].present?
+	end	
+
+	def field_with_icon(icon, &block)
+		capture(&block) + content_tag(:span, content_tag(:i, nil, class: "glyphicon glyphicon-#{icon}"), class: 'input-group-addon') 	
+	end			
+
+	def bootstrap_group_for(form, field, field_class, icon, &block)
+		has_error = form.object.errors[field].present?
+	
+		content_tag :div, class: "form-group #{'has-error' if has_error}"  do
+			concat label_tag(field, Ride.human_attribute_name(field).titleize, class: 'control-label')
+			concat content_tag :div, field_with_icon(icon, &block), class: "input-group datetime #{field_class}"
+			concat errors_for(form,field)
+		end
+	end
 end
+
+
--- app/models/ride.rb
+++ app/models/ride.rb
@@ -2,4 +2,10 @@ class Ride < ActiveRecord::Base
 	belongs_to :user
 	has_many :requests
 	has_many :bookings
+
+	validates :source, :destination, presence: true, length: { minimum: 3 }
+	validates :price, :spots_available, :leaving_date, :leaving_time, presence: true
+	validates :spots_available, :price, numericality: {greater_than: 0}
+
+
 end
