585078ce4c9133fd69f032852bdc78b7b9e83e22
Praveen <ppraveen25@gmail.com>
2015-02-26 23:49:05 -0500

    clean up
    clean up

--- config/environments/production.rb
+++ config/environments/production.rb
@@ -20,7 +20,7 @@ Rails.application.configure do
   # config.consider_all_requests_local       = false 
   # config.action_controller.perform_caching = true
 
-  config.action_mailer.default_url_options = { :host => "https://qrideshare.herokuapp.com" }
+  config.action_mailer.default_url_options = { :host => "qrideshare.herokuapp.com" }
 
   # Enable Rack::Cache to put a simple HTTP cache in front of your application
   # Add `rack-cache` to your Gemfile before enabling this.
